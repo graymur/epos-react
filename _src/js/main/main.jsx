1731ab1c34b857ba96ebc200c17ac50851cf736a
graymur <graymur@mail.ru>
2016-02-11 14:26:44 +0300

    remove comments

--- _src/js/main/main.jsx
+++ _src/js/main/main.jsx
@@ -4,41 +4,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import configureStore from './redux/configureStore.js';
-
 import Root from './Root.jsx';
-
-
-/* -------------------------------- */
-
-//import { createStore, compose } from 'redux';
-//import { persistState } from 'redux-devtools';
-////import rootReducer from '../reducers';
-//import DevTools from './containers/DevTools';
-//
-//const enhancer = compose(
-//    DevTools.instrument(),
-//    persistState(
-//        window.location.href.match(
-//            /[?&]debug_session=([^&]+)\b/
-//        )
-//    )
-//);
-//
-//function configureStore(initialState) {
-//    const store = createStore(rootReducer, initialState, enhancer);
-//
-//    if (module.hot) {
-//        module.hot.accept('../reducers', () =>
-//                store.replaceReducer(require('../reducers').default)
-//        );
-//    }
-//
-//    return store;
-//}
-
-
-/* -------------------------------- */
-
 
 const store = configureStore(window.__INITIAL_STATE__);
 
