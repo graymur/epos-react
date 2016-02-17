c6189c8af331c56c536a20422ec5396492004d93
graymur <graymur@mail.ru>
2016-02-17 18:52:09 +0300

    lightbox

--- _src/js/main/modules/gallery/components/Image.jsx
+++ _src/js/main/modules/gallery/components/Image.jsx
@@ -1,16 +1,42 @@
 import React from 'react';
+import ImageFull from './ImageFull.jsx';
+import Modal from 'react-modal';
+import modalStyles from '../../../util/modal-styles.js';
 
-const Image = ({ source }) => {
-    return (
-        <a href={'/files/' + source} target="_blank">
-            <picture>
-                <source media="(min-width: 769px)" srcSet={'/resize/w150-h150-tput_out-fgs/' + source}/>
-                <source media="(min-width: 401px)" srcSet={'/resize/w768-fgs/' + source}/>
-                <source media="(min-width: 0)" srcSet={'/resize/w400-fgs/' + source}/>
-                <img src={'/resize/w150-h150-tput_out-fgs/' + source}/>
-            </picture>
-        </a>
-    );
-};
+export default class Image extends React.Component {
+    constructor(props) {
+        super(props);
 
-export default Image;
+        this.state = {
+            opened: false
+        }
+    }
+
+    toggleOpened(event) {
+        if (event && event.preventDefault) {
+            event.preventDefault();
+        }
+
+        this.setState({
+            opened: !this.state.opened
+        });
+    }
+
+    render() {
+        let source = this.props.source;
+
+        return (
+            <a href={'/files/' + source} onClick={this.toggleOpened.bind(this)}>
+                <picture>
+                    <source media="(min-width: 769px)" srcSet={'/resize/w150-h150-tput_out-fgs/' + source}/>
+                    <source media="(min-width: 401px)" srcSet={'/resize/w768-fgs/' + source}/>
+                    <source media="(min-width: 0)" srcSet={'/resize/w400-fgs/' + source}/>
+                    <img src={'/resize/w150-h150-tput_out-fgs/' + source}/>
+                </picture>
+                <Modal isOpen={this.state.opened} onRequestClose={this.toggleOpened.bind(this)} style={modalStyles}>
+                    <ImageFull source={source} onClick={this.toggleOpened.bind(this)}/>
+                </Modal>
+            </a>
+        );
+    }
+}
---
+++ _src/js/main/util/modal-styles.js
@@ -0,0 +1,25 @@
+const modalStyles = {
+    overlay: {
+        backgroundColor: ''
+    },
+    content : {
+        top: '',
+        left: '',
+        right: '',
+        bottom: '',
+        marginRight: '',
+        transform: '',
+        borderRadius: '',
+        border: '',
+        overflow: '',
+        padding: '',
+        background: '',
+        position: ''
+    }
+};
+
+if (Object.freeze) {
+    Object.freeze(modalStyles);
+}
+
+export default modalStyles;
